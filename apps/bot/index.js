a7423ade2ed24681e6bce1cb28e9f700a79cb453
Brandon Chambers <bran.cham@gmail.com>
2025-07-20 13:03:10 -0600

    Add @botname preamble requirement for directives

--- apps/bot/index.js
+++ apps/bot/index.js
@@ -10,10 +10,13 @@ import { handlePlayerCommand } from './command-router.js';
 //master player
 const commanderUUID = 'd32f0358-7604-3be7-b35b-6f8e6ec02e05'
 
+//bot's name
+const botName = 'BuilderBot'
+
 const bot = mineflayer.createBot({
   host: '127.0.0.1',
   port: 25565,
-  username: 'BuilderBot',
+  username: botName,
   version: '1.20.4'
 });
 
@@ -38,15 +41,37 @@ bot.once('spawn', async () => {
 });
 
 bot.on('chat', async (username, message) => {
-  if (username === bot.username) return;
+  try {
+    console.log(`Got "${message}" from ${username}`)
 
-  const player = Object.entries(bot.players).filter(([username, user]) => user.uuid === commanderUUID)[0]
+    // this shouldn't ever happen, but just in case the AI starts chatting with users
+    if (username === bot.username) {
+      console.log(`Ignoring chat from self.`)
+      return;
+    }
 
-  // don't allow commands from other players
-  if(player === undefined) {
-    console.log('ignoring chat from unauthorized entity')
-    return
+    // only respond to commands with format like: "@BuilderBot build a fortress"
+    if (!message.includes(`@${botName}`)) {
+      console.log(`Ignoring chat without @${botName}.`)
+      return;
+    }
+
+    const player = Object.entries(bot.players).filter(([username, user]) => user.uuid === commanderUUID)[0]
+
+    // don't allow commands from other players
+    if (player === undefined) {
+      console.log(`Ignoring chat from non-commander entity. Commander uuid is ${commanderUUID}`)
+      return
+    }
+
+    //remove the "@{botName} " part since we've verified it's directed towards the bot.
+    const finalMessage = message.slice(botName.length + 2, message.length)
+
+    //Allowing command
+    console.log(`Allowing "${finalMessage}" from ${username}`)
+
+    await handlePlayerCommand(bot, finalMessage, username);
+  } catch (error) {
+    console.error(`Could not process command. ${error.stack}`)
   }
-
-  await handlePlayerCommand(bot, message, username);
 });
