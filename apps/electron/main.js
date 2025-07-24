e93dc208812845df08d77e7c80bfb63ba13a6e9a
Brandon Chambers <bran.cham@gmail.com>
2025-07-24 15:35:40 -0600

    Forgot the import

--- apps/electron/main.js
+++ apps/electron/main.js
@@ -2,6 +2,7 @@ import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { spawn } from 'child_process';
+import { randomUUID } from 'crypto';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
